1cc8fda2da4c116c25d252d5b21f0487cd4c2a2b
Rxtje <lostlux@gmail.com>
2019-06-11 21:27:13 +0800

    Updates

---
+++ precache-manifest.a28c31db3011ca4d5c510ad30a7a4771.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ab5195a3e35672ca5e0d90b7844373",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "7f8c5400b17fb484f856",
+    "url": "/tetris/static/css/main.a91f8da5.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/tetris/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "7f8c5400b17fb484f856",
+    "url": "/tetris/static/js/main.b2584253.chunk.js"
+  },
+  {
+    "revision": "e9e58439d3b8c64431c2",
+    "url": "/tetris/static/js/runtime~main.61707ba2.js"
+  }
+]);
